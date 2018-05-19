87a70bc55546bff49bde00bb92a215b6aca5d6b6
chechiLiu <39393735+chechiLiu@users.noreply.github.com>
2018-05-18 22:00:16 -0700

    Add files via upload

---
+++ Assignment 0/Firstjavafile.java
@@ -0,0 +1,12 @@
+Che-Chi Jack Liu
+V00850558
+
+public class Simple {
+	public static void main(String[] args) {
+		System.out.println("This class is simple.");
+		fish();
+	}
+	public static void fish() {
+		System.out.println("Red fish \"Blue\" fish!");
+	}
+}
