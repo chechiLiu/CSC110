90eeacd0615912f9295a51f26691f926583a0a74
chechiLiu <39393735+chechiLiu@users.noreply.github.com>
2018-05-18 21:34:41 -0700

    Add files via upload

---
+++ FirstAssignment.java
@@ -0,0 +1,55 @@
+public class FirstAssignment {
+	public static void main(String[] args) {
+		System.out.println("Welcome!");
+		System.out.println("\n");
+		printTotemPole();
+		calcSurfaceArea();
+	}
+	
+	public static void printPig() {
+		System.out.println("  ^--^");
+		System.out.println(" (0  0)");
+		System.out.println("  (oo)");
+		System.out.println(" (\")_(\")@");
+	}
+	
+	public static void printFrog() {
+		System.out.println("  @..@");
+		System.out.println(" (----)");
+		System.out.println("( >__< )");
+		System.out.println("\"\"    \"\"");
+	}
+	
+	public static void printTotemPole() {
+		System.out.println("/~~~~~~\\");
+		printFrog();
+		System.out.println("/~~~~~~\\");
+		printPig();
+		System.out.println("/~~~~~~\\");
+		printFrog();
+		System.out.println("/~~~~~~\\");
+		printPig();
+		System.out.println("/~~~~~~\\");
+		System.out.println("/~~~~~~\\");
+	}
+	
+	public static void calcSurfaceArea() {
+		System.out.println("\n");
+		int height = 5;
+		int diameter = 4;		
+		int radius = 2;
+		double CircumFerence = 2 * Math.PI * radius;
+		double AreaOfRectangle = CircumFerence * height;
+		double TotalAreaOfACircle = Math.PI * radius * radius;
+		double TotalSurfaceArea = AreaOfRectangle + TotalAreaOfACircle + TotalAreaOfACircle;
+		System.out.println("Total surface area is: "+ TotalSurfaceArea);
+	}
+
+	/* 
+* V00850558 Che-Chi Liu, Jack Liu
+* Computer Sciences 110 FirstAssignment
+* Java program that produced totem pole and
+* calculated area of a cylinder as output
+*/
+	
+}
