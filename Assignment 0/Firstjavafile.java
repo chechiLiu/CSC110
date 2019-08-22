bd36666d1282d70864383a02d18b52f2e102bc3c
Jack Liu <chechiliu@outlook.com>
2019-08-22 16:04:37 -0700

    Update Firstjavafile.java

--- Assignment 0/Firstjavafile.java
+++ Assignment 0/Firstjavafile.java
@@ -1,11 +1,12 @@
-//Che-Chi Jack Liu
+//Che-Chi (Jack) Liu
 //V00850558
 
-public class Simple {
+public class Firstjavafile {
 	public static void main(String[] args) {
 		System.out.println("This class is simple.");
 		fish();
 	}
+	
 	public static void fish() {
 		System.out.println("Red fish \"Blue\" fish!");
 	}
